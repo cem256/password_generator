fcb72f9935db68b0624d5cf4fe57ad56bafb003e
cem256 <cemo99@gmail.com>
2022-08-08 23:10:25 +0300

    Fixed a bug where privacy policy was redirected to wrong link

--- lib/product/constants/string_constants.dart
+++ lib/product/constants/string_constants.dart
@@ -1,4 +1,4 @@
 class StringConstants {
   static const String appName = "Password Generator";
-  static const String privacyPolicyUrl = "https://github.com/cem256";
+  static const String privacyPolicyUrl = "https://github.com/cem256/Password_Generator/blob/master/privacy-policy.md";
 }
