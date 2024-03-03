bc0110296fb7add91446f66c7624aa5ff7820dc8
cem256 <cemo99@gmail.com>
2024-03-03 13:34:49 +0300

    add discover more app section

--- lib/app/constants/asset_constants.dart
+++ lib/app/constants/asset_constants.dart
@@ -1,3 +1,4 @@
 abstract final class AssetConstants {
   static const String appIcon = 'assets/icons/generator_no_bg.png';
+  static const String gptDetectorAppIcon = 'assets/icons/gpt_detector_no_bg.png';
 }
--- lib/app/constants/string_constants.dart
+++ lib/app/constants/string_constants.dart
@@ -1,6 +1,8 @@
 abstract final class StringConstants {
   static const String appName = 'Password Generator';
+  static const String gptDetector = 'GPT Detector';
   static const String githubUrl = 'https://github.com/cem256/password_generator';
   static const String privacyPolicyUrl = 'https://cem256.github.io/password_generator/privacy-policy.html';
   static const String googlePlayUrl = 'https://play.google.com/store/apps/details?id=com.cem256.passwordgenerator';
+  static const String gptDetectorPlayStoreUrl = 'https://play.google.com/store/apps/details?id=com.cem256.gptdetector';
 }
